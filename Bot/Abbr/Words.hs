3cc3642f57cdace82cc8519d287430ed4457ae5f
Raitis Stengrevics <dagrevis@gmail.com>
2014-07-30 20:47:21 +0300

    Add more abbrs, sort them

--- Bot/Abbr/Words.hs
+++ Bot/Abbr/Words.hs
@@ -5,6 +5,10 @@ where
 abbr :: [(String, String)]
 abbr = 
     [
-        ("til", "today I learned"),
-        ("afaik", "as far as I know")
+        ("afaik", "as far as I know"),
+        ("imho", "in my humble opinion"),
+        ("imo", "in my opinion"),
+        ("lol", "laughing out loud"),
+        ("omg", "oh my god"),
+        ("til", "today I learned")
     ]
