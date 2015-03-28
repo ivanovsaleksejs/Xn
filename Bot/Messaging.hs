3ffd5d3951794ab8ab5cd4679cc00236e6002a0e
siers <wimuan@gmail.com>
2015-03-28 23:54:11 +0200

    Declare a type for commands to make changes to it visible.

--- Bot/Messaging.hs
+++ Bot/Messaging.hs
@@ -24,6 +24,7 @@ import Bot.Commands.Rand
 import Bot.Commands.Time
 import Bot.Commands.URL
 
+commands :: [(String -> Bool, String -> MessageStack -> Net ())]
 commands =
     [
             (s' . clean, substmsg), -- Substitute
